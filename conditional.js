3730fe2db0db254a6310ad958014e679e218a518
nitindiwakar123 <nitindiwakar2006@gmail.com>
2025-01-21 22:17:51 +0530

    Create conditional.js

---
+++ conditional.js
@@ -0,0 +1,44 @@
+//Conditional Statements in JavaScript
+
+//if-else statements
+/*
+let a=60;
+let b=30;
+if(a>b){
+    console.log("a is greater than b"); //executed if the condition is true
+}
+else if(a>=b){
+    console.log("b is greater than a"); //executed if the condition is true
+}
+else{
+    console.log("Given Condition is false"); //executed if none of the above condition is true
+}
+*/
+
+//Switch Statements
+/*
+let age=7;
+switch (age) {
+    case 0:
+        console.log("Invalid age"); //code to be executed if age === 0
+        break;
+
+    case 10:
+        console.log("Your age in 10y/o"); //code to be executed if age === 10
+        break;
+
+    case 18:
+        console.log("your age is 18y/o"); //code to be executed if age === 18
+        break;
+
+    default:
+        console.log("your age is not matched to our eligible ages 10 and 18");  // code to be executed if none of the above cases match
+}
+*/
+
+//Ternary Operator
+//JavaScript Ternanry Operator is similar to if else, it evaluates a condition and execute a block of code based on the condition
+/*
+let marks = 33;
+console.log("you are", ((marks >= 33) ? "passed" : "failed")); //if codition is true then prints passed otherwise failed
+*/
