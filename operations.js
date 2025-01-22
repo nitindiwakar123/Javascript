07c9513c9c24eb5299058344027a48cf892bef9c
nitindiwakar123 <nitindiwakar2006@gmail.com>
2025-01-22 14:41:35 +0530

    Create operations.js

---
+++ operations.js
@@ -0,0 +1,61 @@
+// let v = 6
+// let nb = -v
+// console.log(nb)
+
+// let str="helo"
+// let str1="hulla"
+// let str3 = str + " " + str1; //string concation
+// console.log(str3);
+
+// console.log(3 + 2 + "2"); //first it add 3+2=5 as number then 5+"2" = 52, because number is before string
+// console.log("1" + "2" + 3); //it add "1"+"2"+3 as string, because number is after string
+
+// console.log((5%5) + 8 * 7) //don't do this, it decreases readability of code
+
+// console.log(+true) //output 1
+// console.log(true+) //output error
+// console.log(+"")
+
+// let n, n1, n2;
+
+// n = n1 = n2 = 5+7; //don't do this
+// console.log(n);
+// console.log(n1);
+// console.log(n2);
+
+// let gameCounter = 100;
+// ++gameCounter;
+// console.log(gameCounter);
+
+//Comparison of Datatypes
+// console.log(2>6);
+// console.log(2>=6);
+// console.log(2<6);
+// console.log(2<=6);
+// console.log(2==6);
+// console.log(2!=6);
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0); //The Reason is that an equality check == and comparisons >, <, >=, <= work differently because comparisons convert null to a number, treating it as 0. That's thy null>=0 is true and null>0 is false.
+// console.log(null>=true); 
+// console.log(null!=0);
+// console.log(null!==0);
+// console.log(null===0);
+// console.log(null===true);
+
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+// console.log(undefined === 0);
+// console.log(undefined != 0);
+// console.log(undefined !== 0);
+ // console.log(undefined === false);
+ // console.log(undefined === null);
+ // console.log(undefined !== null);
+ 
+
