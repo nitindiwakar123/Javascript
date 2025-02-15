63f9518691a12c507cc111dd88d046f4ebcce755
nitindiwakar123 <nitindiwakar2006@gmail.com>
2025-02-15 10:15:53 +0530

    Create Global_and_Block_Scope.js

---
+++ Global_and_Block_Scope.js
@@ -0,0 +1,57 @@
+
+// let a = 100
+// const b = 200
+// var c = 300 
+
+// if (true) {
+//    let a = 10;
+//     const b = 20;
+//     var c = 30;
+//     console.log("Inner: ", a);
+//     console.log("Inner: ", b);
+//     console.log("Inner: ", c);
+// }
+
+// console.log("Outer: ", a);
+// console.log("Outer: ", b);
+// console.log("Outer: ", c);
+
+/*
+function one() {
+    const name = "Nitin";
+    function two() {
+        const web = "Yt"
+        console.log(name);
+    }
+    console.log(web); //this will throw an error
+
+    // two(); 
+}
+
+one();
+*/
+/*
+if (true) {
+    const username = "Hitesh";
+    
+    if (username === "Hitesh") {
+        const website = " Youtube";
+        console.log(username + website);
+    }
+
+    // console.log(website); //this will throw an error 
+}
+// console.log(username); //this will  throw an error
+*/
+
+/*
+console.log(addOne(6)); //this will not throw an error
+function addOne(num){
+    return num+1;
+}
+
+console.log(addTwo(8)); //this will throw an error
+const addTwo = function(num){
+    return num+2;
+}
+*/
